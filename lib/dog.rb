883ba934d5494bdc8d0f91bd3b95c65c55056419
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-11 14:07:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all #class method for reading data stored in class variable @@all
+   @@all
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear #using Array#clear method
+  end
+
+end
